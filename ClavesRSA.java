a6f280097844cf4b299bceb955bb441b94a2d55e
nehemias <nemiasplata@yahoo.com.mx>
2020-11-25 18:33:45 -0600

    Pruebas Exitosas

--- ClavesRSA.java
+++ ClavesRSA.java
@@ -1,3 +1,12 @@
 public class ClavesRSA{
 	
+	private BigInteger clave_P;
+	private BigInteger clave_Q;
+	private BigInteger clave_Phi;
+
+	public ClavesRSA(BigInteger p, BigInteger q, BigInteger phi){
+		this.clave_P = p;
+		this.clave_Q = q;
+		this.clave_Phi = phi;
+	}
 }
--- GuardarClaves.java
+++ GuardarClaves.java
@@ -1,4 +1,13 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class GuardarClaves implements Serializable{
 	
+	private ArrayList<ClavesRSA> listaClavesRSA = new ArrayList<ClavesRSA>();
+	private ClavesRSA claves = new ClavesRSA();
+
+	public GuardarClaves
+
 }
